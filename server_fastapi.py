c0769e2243fba47e8bce4bc000c765f4b77a6b1d
CamiloCuenca <camilocuenca1810@gmail.com>
2025-11-08 17:35:52 -0500

    Add localhost origin to CORS middleware for development

--- server_fastapi.py
+++ server_fastapi.py
@@ -44,6 +44,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:8000",
+        "http://127.0.0.1:5500",
         "https://proyecto-analisis-algoritmos-nine.vercel.app"
     ],
     allow_credentials=True,
